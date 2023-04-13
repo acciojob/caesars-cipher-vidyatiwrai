a1ba322000f5070135a376ca6499c85566b068f1
vidyatiwrai <127404171+vidyatiwrai@users.noreply.github.com>
2023-04-13 16:54:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -13,6 +13,19 @@ const lookup = {
 function rot13(encodedStr){
    let decodedArr = []; // Your Result goes here
   // Only change code below this line
+	function rot13(str) {
+var decoded = "";
+
+for (var i = 0; i < str.length; i++) {
+var code = str.charCodeAt(i);
+if (code >= 65 && code <= 90) {
+code = ((code - 65 + 13) % 26) + 65;
+}
+decoded += String.fromCharCode(code);
+}
+
+return decoded;
+}
 
   return ;//return decodedArr
 }
