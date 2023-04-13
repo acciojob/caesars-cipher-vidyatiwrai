3f805cf47258ebc10f5d06b447446d106139be3f
vidyatiwrai <127404171+vidyatiwrai@users.noreply.github.com>
2023-04-13 17:04:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 // Your Script here.
 
-const lookup = {
-  'A': 'N','B': 'O','C': 'P','D': 'Q',
-  'E': 'R','F': 'S','G': 'T','H': 'U',
-  'I': 'V','J': 'W','K': 'X','L': 'Y',
-  'M': 'Z','N': 'A','O': 'B','P': 'C',
-  'Q': 'D','R': 'E','S': 'F','T': 'G',
-  'U': 'H','V': 'I','W': 'J','X': 'K',
-  'Y': 'L','Z': 'M', '?': '?', ',': ','
-};
+// const lookup = {
+//   'A': 'N','B': 'O','C': 'P','D': 'Q',
+//   'E': 'R','F': 'S','G': 'T','H': 'U',
+//   'I': 'V','J': 'W','K': 'X','L': 'Y',
+//   'M': 'Z','N': 'A','O': 'B','P': 'C',
+//   'Q': 'D','R': 'E','S': 'F','T': 'G',
+//   'U': 'H','V': 'I','W': 'J','X': 'K',
+//   'Y': 'L','Z': 'M', '?': '?', ',': ','
+// };
 
-function rot13(encodedStr){
-   let decodedArr = []; // Your Result goes here
+// function rot13(encodedStr){
+//    let decodedArr = []; // Your Result goes here
   // Only change code below this line
 	function rot13(str) {
 var decoded = "";
@@ -27,8 +27,8 @@ decoded += String.fromCharCode(code);
 return decoded;
 }
 
-  return ;//return decodedArr
-}
+  // return ;//return decodedArr
+// }
 
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
