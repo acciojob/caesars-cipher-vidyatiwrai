56010af0751a663866192c0f79120a8e57d802be
Vidya Tiwari <127404171+vidyatiwrai@users.noreply.github.com>
2023-05-11 15:15:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,4 @@
-// Your Script here.
 
-// const lookup = {
-//   'A': 'N','B': 'O','C': 'P','D': 'Q',
-//   'E': 'R','F': 'S','G': 'T','H': 'U',
-//   'I': 'V','J': 'W','K': 'X','L': 'Y',
-//   'M': 'Z','N': 'A','O': 'B','P': 'C',
-//   'Q': 'D','R': 'E','S': 'F','T': 'G',
-//   'U': 'H','V': 'I','W': 'J','X': 'K',
-//   'Y': 'L','Z': 'M', '?': '?', ',': ','
-// };
-
-// function rot13(encodedStr){
-//    let decodedArr = []; // Your Result goes here
-  // Only change code below this line
 const lookup = {
     'A': 'N','B': 'O','C': 'P','D': 'Q',
     'E': 'R','F': 'S','G': 'T','H': 'U',
@@ -34,28 +20,4 @@ const lookup = {
   }
   return outputString;
   }
-
-
-
-// 	function rot13(str) {
-// var decoded = "";
-
-// for (var i = 0; i < str.length; i++) {
-// var code = str.charCodeAt(i);
-// if (code >= 65 && code <= 90) {
-// code = ((code - 65 + 13) % 26) + 65;
-// }
-// decoded += String.fromCharCode(code);
-// }
-
-// return decoded;
-// }
-
-  // return ;//return decodedArr
-// }
-
-// You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
-
-// console.log(rot13("SERR YBIR? NPPVBWBO"));
-
 module.exports = rot13;
